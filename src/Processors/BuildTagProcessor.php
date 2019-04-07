9c4240c661a80aaa0573a9ea9f6bdf8cc9c1ed21
jradtilbrook <jrad.tilbrook@gmail.com>
2019-04-07 20:03:38 +0800

    Fix typo implementing interface method

--- src/Processors/BuildTagProcessor.php
+++ src/Processors/BuildTagProcessor.php
@@ -7,16 +7,16 @@ use Monolog\Processor\ProcessorInterface;
 class BuildTagProcessor implements ProcessorInterface
 {
     /**
-     * @param  array $record
+     * @param  array $records
      * @return array
      */
     public function __invoke(array $records)
     {
         // add the docker build tag if present
         if (config('app.build_tag')) {
-            $record['extra']['build_tag'] = config('app.build_tag');
+            $records['extra']['build_tag'] = config('app.build_tag');
         }
 
-        return $record;
+        return $records;
     }
 }
